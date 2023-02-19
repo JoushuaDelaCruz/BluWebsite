d395aaea7ecad54c8e5acf4700898fb3041ec514
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-02-18 19:56:44 -0800

    Finished designing the header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom'
 /* This contains all elements in the header */
 const Header = () => {
     return (
-        <header className='inline-flex p-4 justify-between justify-items-center min-w-full 
-                            items-center font-serif text-lg font-bold bg-neutral-50 text-blue-600'>
-            <Link to='/' className='uppercase'> Blu </Link>
-            <Link to='/books'> Books </Link>
-            <Link to='/about'> About </Link>
+        <header className='header'>
+            <nav className='nav-bar'>
+                <Link to='/' className='uppercase cursor-pointer stroke-current'> Blu </Link>
+                <Link to='/books' className='cursor-pointer'> Books </Link>
+                <Link to='/news' className='cursor-pointer'> News </Link>
+            </nav>
         </header>
     )
 }
