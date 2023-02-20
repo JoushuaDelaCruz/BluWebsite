b8fa55237637f00b45ff2a0ba99aef0b7afde0f4
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-02-19 16:57:04 -0800

    Fixed the formatting of header code

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 /* This contains all elements in the header */
 const Header = () => {
-    return (
-        <header className='header'>
-            <nav className='nav-bar'>
-                <Link to='/' className='uppercase cursor-pointer stroke-current'> Blu </Link>
-                <Link to='/books' className='cursor-pointer'> Books </Link>
-                <Link to='/news' className='cursor-pointer'> News </Link>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header className="header">
+      <nav className="nav-bar">
+        <Link to="/" className="uppercase cursor-pointer stroke-current">
+          {" "}
+          Blu{" "}
+        </Link>
+        <Link to="/books" className="cursor-pointer">
+          {" "}
+          Books{" "}
+        </Link>
+        <Link to="/about" className="cursor-pointer">
+          {" "}
+          About{" "}
+        </Link>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
