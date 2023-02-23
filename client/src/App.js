86acf1acedc5641c47144367f883b9568147b32f
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-02-22 20:11:22 -0800

    Deleted unnecessary code

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
 
 const App = () => {
-  function handleCallbackResponse(response) {
-    console.log(response.credential);
-  }
-
-  useEffect(() => {
-    /*global google*/
-    google.accounts.id.initialize({
-      client_id:
-        "616141306078-pl6mggut4lul7liot0ql6jjomof1k8kk.apps.googleusercontent.com",
-      callback: handleCallbackResponse,
-    });
-
-    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
-      theme: "outlie",
-      size: "large",
-    });
-  }, []);
-
   return (
     <>
       <div id="signInDiv"></div>
